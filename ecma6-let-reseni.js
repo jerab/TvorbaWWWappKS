ac246232567945c7f9172dfbb3cba6b304be655b
Tomas Jerab <t.jerab@gmail.com>
2017-11-02 17:29:56 +0100

    Pouziti let a var

---
+++ ecma6-let-reseni.js
@@ -0,0 +1,12 @@
+var jmeno = "Josef";
+let prijmeni = "Koudelka";
+var i = 3;
+
+/// TODO: kdyz je i == vnitrnimu i zmen Prijmeni
+for(let i = 0; i <= 10; i++) {
+	if(i == this.i) {
+		prijmeni = "Procházka";
+	}
+	console.log(i, this.i, jmeno, prijmeni);
+}
+console.log(i, jmeno, prijmeni);
---
+++ ecma6-let.js
@@ -0,0 +1,10 @@
+/// TODO: jmeno, prijmeni, i
+
+/// TODO: cyklus s i od 0 do 10
+for(let i = 0; i <= 10; i++) {
+	// kdyz je i == vnitrnimu i zmen Prijmeni
+
+	//TODO: vypis i, jmeno a prijmeni
+}
+//TODO: vypis i, jmeno a prijmeni
+
